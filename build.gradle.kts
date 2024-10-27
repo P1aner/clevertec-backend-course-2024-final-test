069b30dfdb15c6bbf15343cd12810f8c7a7da2c5
Pavel Leshchanka <pavel.leshchanka@gmail.com>
2024-10-27 03:32:07 +0300

    Liquibase migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApi")
-    //   implementation("org.liquibase:liquibase-core")
+    implementation("org.liquibase:liquibase-core")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
